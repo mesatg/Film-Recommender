a9195a0b735d7cf95cad5ed2ec5e971f0d438b67
mesatg <gozutokesat2002@gmail.com>
2025-02-24 23:59:17 +0300

    update

--- app.py
+++ app.py
@@ -61,4 +61,5 @@ def recommend():
     return jsonify({'main_poster': main_poster, 'recommended': recommended})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
