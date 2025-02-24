c8e825873eb508557cdd0f9a6fc0f75469ba8274
mesatg <gozutokesat2002@gmail.com>
2025-02-25 00:43:34 +0300

    update

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template, request, jsonify
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import ast
+import gdown
+import os
 import joblib
 import requests
 
 app = Flask(__name__)
 
+# Google Drive üzerinden model dosyasını indirme
+MODEL_PATH = 'film_recommender_model.pkl'
+FILE_ID = '1UcZ7JVPsjcGmV_1Bkmr6LmqmQtkDzdJj'  # Google Drive'dan alınan ID
+if not os.path.exists(MODEL_PATH):
+    gdown.download(f'https://drive.google.com/uc?id={FILE_ID}', MODEL_PATH, quiet=False)
+
+# Model ve veri setini yükle
+movies, similarity = joblib.load(MODEL_PATH)
+
+# Film isimlerini küçük harfe çevir
+df_titles = movies['original_title'].str.lower().tolist()
+
+# TMDB API ayarları
 API_KEY = 'b38a1d89382721590ac0c691832bed08'
 BASE_URL = 'https://api.themoviedb.org/3'
 IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500'
 
-# 🎬 Eğitilen modeli yükle
-movies, similarity = joblib.load('film_recommender_model.pkl')
-df_titles = movies['original_title'].str.lower().tolist()
-
-# 🎞️ Film posteri alma fonksiyonu
+# Film posteri alma fonksiyonu
 def get_movie_poster(title):
     try:
         response = requests.get(
@@ -29,19 +44,16 @@ def get_movie_poster(title):
         print(f"🚨 API hatası: {e}")
         return '/static/images/default_poster.png'
 
-# 🌐 Ana sayfa
 @app.route('/')
 def home():
     return render_template('index.html', movies=list(movies['original_title'].values))
 
-# 🔍 Otomatik tamamlama
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
     query = request.args.get('query', '').lower()
     suggestions = [title for title in df_titles if query in title][:5]
     return jsonify(suggestions)
 
-# 🎥 Film önerme
 @app.route('/recommend', methods=['POST'])
 def recommend():
     movie = request.json.get('movie').lower()
